efc9a87dac24a9bb9105e47cdb5b56a5ed10f2bb
toothless204 <mynf345@gmail.com>
2026-04-23 21:58:19 +0700

    Add files via upload

---
+++ modul 3/PPST3_Modul 3_SQL_KelompokC3.sql
@@ -0,0 +1,44 @@
+SELECT *
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Diameter Lubang Horizontal part pivot'
+ORDER BY Sample_ke ASC, Observasi_ke ASC;
+
+SELECT *
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Diameter Half cylinder bottom base 2'
+ORDER BY Sample_ke ASC, Observasi_ke ASC;
+
+SELECT *
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Jumlah nonconformity pan assembly'
+ORDER BY Sample_ke ASC, Observasi_ke ASC 
+
+SELECT *
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Jumlah nonconforming (cacat) Tripod'
+ORDER BY Sample_ke ASC, Observasi_ke ASC;
+
+SELECT *
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Panjang Leg Tripod (mm)'
+ORDER BY Sample_ke ASC, Observasi_ke ASC;
+
+SELECT *
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Jumlah saltik (typo) pada dokumen (1 inspeksi unit = 1 dokumen)'
+ORDER BY Sample_ke ASC, Observasi_ke ASC;
+
+SELECT *
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Panjang Part XYZ 1'
+ORDER BY Sample_ke ASC, Observasi_ke ASC;
+
+SELECT*
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Observasi diameter luar (mm)'
+ORDER BY Sample_ke ASC, Observasi_ke ASC 
+
+SELECT*
+FROM dbo.[Dataset PPST III Modul 3 2025]
+WHERE ObjekUkur = 'Observasi diameter dalam (mm)'
+ORDER BY Sample_ke ASC, Observasi_ke ASC
